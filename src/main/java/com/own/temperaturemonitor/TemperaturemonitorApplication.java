1e89d8e16270068e4416c6c97cf8976b4700b416
Alexei Ivanov <aaivanov23@gmail.com>
2021-02-01 08:22:20 +0300

    first commit

---
+++ src/main/java/com/own/temperaturemonitor/TemperaturemonitorApplication.java
@@ -0,0 +1,13 @@
+package com.own.temperaturemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperaturemonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperaturemonitorApplication.class, args);
+	}
+
+}
